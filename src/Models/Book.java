4e4e49b3e57384b79cfeb706750b694d7be03cdd
Your Name <you@example.com>
2025-07-16 16:38:25 -0500

    Eva

--- src/Models/Book.java
+++ src/Models/Book.java
@@ -11,42 +11,27 @@ public class Book {
         this.autor = autor;
         this.anio = anio;
     }
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getAutor() {
-        return autor;
-    }
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-    public int getAnio() {
-        return anio;
-    }
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-    @Override
-    public String toString() {
-        return "Book{" +
-                "titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", anio=" + anio +
-                '}';
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(titulo, anio);
-    }
+
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public int getAnio() { return anio; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Book)) return false;
         Book book = (Book) o;
         return anio == book.anio && Objects.equals(titulo, book.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, anio);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %d", titulo, autor, anio);
     }
     
 
